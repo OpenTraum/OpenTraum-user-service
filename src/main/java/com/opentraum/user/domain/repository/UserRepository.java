4394fe8cb9661fc5ba83342eea67e197669a98a5
mrangjw <157506327+mrangjw@users.noreply.github.com>
2026-03-16 18:51:36 +0900

    fix: #6 Repository import 경로 수정 (컴파일 에러)
    ReactiveCrudRepository import 경로 수정:
    - org.springframework.data.r2dbc.repository.ReactiveCrudRepository (존재하지 않음)
    → org.springframework.data.repository.reactive.ReactiveCrudRepository (정상)
    
    대상: TenantRepository, UserRepository

--- src/main/java/com/opentraum/user/domain/repository/UserRepository.java
+++ src/main/java/com/opentraum/user/domain/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.opentraum.user.domain.repository;
 
 import com.opentraum.user.domain.entity.User;
-import org.springframework.data.r2dbc.repository.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
